c9a7c97b1a875ea645b099eecb69e38201351c64
Stas Masevych <stasmasevych@gmail.com>
2023-10-17 15:07:29 +0300

    change message

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Message from './scenes/Message';
+import App1 from './scenes/App1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Message />
+    <App1 />
   </React.StrictMode>,
 );
